89fd5662fe8c11b780b91f01fa61975cd3117505
Alessandro021 <alessandrodamata6@gmail.com>
2022-08-20 20:12:20 -0300

    Importando image de cronometro , crindo botões  e estilizando

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component} from 'react'
+import { View , Text, Image, SafeAreaView, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
+  
+class App extends Component{
+
+  render(){
+    return(
+      <View style={styles.container}>
+
+        <Image
+          source={require('./src/cronometro.png')}
+          style={styles.cronometro}
+          // style={{width: 100, height: 100}} 
+        />
+
+      <Text style={styles.timer}>0.0</Text>
+
+      <View style={styles.btnArea}>
+
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnTexto}>VAI</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.btn}>
+           
+            <Text style={styles.btnTexto}>LIMPAR</Text>
+          </TouchableOpacity>
+
+      </View>
+
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00aeef",
   },
-});
+  timer:{
+    marginTop: -180,
+    color: "#fff",
+    fontSize: 60,
+    fontWeight: "bold"
+  },
+  btnArea:{
+    flexDirection:"row",
+    marginTop: 120,
+    height: 50,
+  },
+  btn:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+    height: 50,
+    margin: 17,
+    borderRadius: 19
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#FFF"
+  },
+  cronometro:{
+    width: 350, 
+    height: 350,
+  }
+})
+
+export default App;
